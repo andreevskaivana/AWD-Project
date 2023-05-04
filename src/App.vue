fcfa59de05b9734746464b754c868b891e9427d3
andreevskaivana <ivana.andreevska25@gmail.com>
2023-05-04 22:54:29 +0200

    Initial Commit + Navbar component

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Navbar/>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+export default{
+  name:'App',
+  components:{
+    Navbar,
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+          </li>
+        </ul>
+      </nav>
+</template>
+
+<script>
+export default {
+    name:'Navbar'
+}
+</script>
+
+<style>
+
+</style>
