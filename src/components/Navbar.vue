aa774fef0b901f5dad6d6745be34e415003f915a
andreevskaivana <ivana.andreevska25@gmail.com>
2023-05-04 23:12:37 +0200

    Navbar styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,19 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+    <nav class="navbar navbar-expand-lg navbar-light justify-content-between" style="background-color: #0C0C0C;">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
+            <a class="nav-link" href="#" style="color:#69DC9E;">Home</a>
           </li>
         </ul>
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="#">About Us</a>
+            <a class="nav-link" href="#" style="color:#69DC9E;">About Us</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Services</a>
+            <a class="nav-link" href="#" style="color:#69DC9E;">Services</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Contact Us</a>
+            <a class="nav-link" href="#" style="color:#69DC9E;">Contact Us</a>
           </li>
         </ul>
       </nav>
